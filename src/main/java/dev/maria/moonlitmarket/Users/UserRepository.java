4d7766de3ba978f62e5b3086016c5af9e5bdfe2a
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-19 19:26:06 +0100

    done userRepository

--- src/main/java/dev/maria/moonlitmarket/Users/UserRepository.java
+++ src/main/java/dev/maria/moonlitmarket/Users/UserRepository.java
@@ -3,5 +3,4 @@ package dev.maria.moonlitmarket.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Implement methods for user operations
 }
