44fadd3346157698fc7681083a721681b880770c
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-22 19:23:47 +0100

    update UserRepository.java

--- src/main/java/dev/maria/moonlitmarket/Users/UserRepository.java
+++ src/main/java/dev/maria/moonlitmarket/Users/UserRepository.java
@@ -1,6 +1,10 @@
 package dev.maria.moonlitmarket.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public boolean existsByUsername(String username);
 }
