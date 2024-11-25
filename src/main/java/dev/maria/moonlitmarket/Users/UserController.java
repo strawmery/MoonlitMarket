dbef070d493712825a99c79cd3bc5c3e59a1003e
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-25 23:21:29 +0100

    fixed UserController.java

--- src/main/java/dev/maria/moonlitmarket/Users/UserController.java
+++ src/main/java/dev/maria/moonlitmarket/Users/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api")
 public class UserController {
 
-    private final UserService userService;
+    private UserService userService;
 
     public UserController(UserService userService){
         this.userService =userService;
