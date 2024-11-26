22040d75d9647981b2ed980bc70c5df3c75ac508
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-26 22:14:12 +0100

    feat: create ProductsRepository.java

---
+++ src/main/java/dev/maria/moonlitmarket/Products/ProductsRepository.java
@@ -0,0 +1,7 @@
+package dev.maria.moonlitmarket.Products;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<Products, Long> {
+
+}
