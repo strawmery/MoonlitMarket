b6dc5dc7f895c11ad402aa6b7eaefd37afb9ad23
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-26 22:14:23 +0100

    feat: create ProductsDTO.java

---
+++ src/main/java/dev/maria/moonlitmarket/Products/ProductsDTO.java
@@ -0,0 +1,18 @@
+package dev.maria.moonlitmarket.Products;
+
+public class ProductsDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String descrption;
+
+    private double price;
+
+    private String size;
+
+    private Long idCategory;
+
+
+}
