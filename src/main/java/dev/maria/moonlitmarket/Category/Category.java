256087e7a026cc7243e1d9555c40de451e27c4a6
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-30 18:07:34 +0100

    Created Category and CategoryDTO

--- src/main/java/dev/maria/moonlitmarket/Category/Category.java
+++ src/main/java/dev/maria/moonlitmarket/Category/Category.java
@@ -1,5 +1,36 @@
 package dev.maria.moonlitmarket.Category;
 
+import java.util.List;
+
+import dev.maria.moonlitmarket.Products.Products;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
 public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private List<Products> products;
+
+
 
 }
---
+++ src/main/java/dev/maria/moonlitmarket/Category/CategoryDTO.java
@@ -0,0 +1,8 @@
+package dev.maria.moonlitmarket.Category;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+}
