2bdc07a7382a4c6d4b42986c899d6ce2adc4aa43
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-03 22:03:36 +0100

    done CategoryDTO.java

--- src/main/java/dev/maria/moonlitmarket/Category/CategoryDTO.java
+++ src/main/java/dev/maria/moonlitmarket/Category/CategoryDTO.java
@@ -1,8 +1,11 @@
 package dev.maria.moonlitmarket.Category;
 
+import java.util.List;
+
 public class CategoryDTO {
 
     private Long id;
     private String name;
+    private List<Long> productsId;
 
 }
