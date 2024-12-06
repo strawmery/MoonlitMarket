f0f3602e077799cf175612e87a6587798934e657
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-06 17:35:32 +0100

    feat: create CategoryRepository.java

---
+++ src/main/java/dev/maria/moonlitmarket/Category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package dev.maria.moonlitmarket.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
