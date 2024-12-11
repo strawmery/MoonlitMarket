22ac9fa86663cead4e91dc20af8f3b0546f1a009
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-11 17:52:52 +0100

    feat: create OrdersRepository.java finished

---
+++ src/main/java/dev/maria/moonlitmarket/Orders/OrdersRepository.java
@@ -0,0 +1,7 @@
+package dev.maria.moonlitmarket.Orders;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+
+}
