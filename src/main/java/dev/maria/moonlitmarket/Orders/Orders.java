6b8b8caa696e70e8ca21d30589f3699cc1d420ea
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-11 17:51:34 +0100

    feat: create Orders.java finished

---
+++ src/main/java/dev/maria/moonlitmarket/Orders/Orders.java
@@ -0,0 +1,46 @@
+package dev.maria.moonlitmarket.Orders;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+
+import dev.maria.moonlitmarket.Products.Products;
+import dev.maria.moonlitmarket.Users.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<Products> products;
+
+    private String status;
+
+    private LocalDateTime orderDate;
+
+}
